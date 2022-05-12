8d1e5428d80774b03bf8c5b7276737c23b22c604
Carro <robertmyers@hey.com>
2022-05-12 14:49:01 -0500

    return api

--- index.ts
+++ index.ts
@@ -5,6 +5,20 @@ const { Subtensor } = require('./src/lib/_subtensor/index.ts');
 const substrate_host = 'ws://archivelb.nakamoto.opentensor.ai:9944';
 
 const subtensor = new Subtensor("nakamoto", substrate_host);
-subtensor.create().then(()=>{
-    console.log(`subtensor ${substrate_host} connected`);
-});
+// subtensor.create().then(()=>{
+//     console.log(`subtensor ${substrate_host} connected`);
+// });
+
+
+const connect_to_subtensor = async () => {
+    const api = await subtensor.create()
+    return api;
+}
+
+const run = async () => {
+    const sub = await connect_to_subtensor();
+
+    console.log(sub);
+}
+
+run();
